abf24a591f625335b846a6fed7842c31019a983e
CHOO <CHOO@DESKTOP-16G3G4B>
2020-03-11 21:33:28 +0900

    주석 하나 추가 action

--- src/action/Action.java
+++ src/action/Action.java
@@ -6,3 +6,4 @@ import javax.servlet.http.HttpServletResponse;
 public interface Action {
 	public ActionForward execute(HttpServletRequest req, HttpServletResponse res) throws Exception;
 }
+// 테스트를 위해 주석 추가
